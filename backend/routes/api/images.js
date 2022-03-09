d45fbaef4840e9c1ab68da027010f22014d6c6cb
Jason Li <jasonjli218@gmail.com>
2022-03-09 09:24:41 -0800

    finished backend?

--- backend/routes/api/images.js
+++ backend/routes/api/images.js
@@ -2,16 +2,15 @@ const express = require('express');
 const asyncHandler = require('express-async-handler');
 
 const { requireAuth } = require('../../utils/auth');
-
-
 const db = require('../../db/models');
+
+const imageValidations = require('../../utils/images');
 
 const router = express.Router();
 
 router.get('/', asyncHandler(async (req, res) => {
     const image = await db.Image.findAll();
     return res.json(image);
-
 }));
 
 router.get('/:id', asyncHandler(async (req, res) => {
@@ -27,9 +26,30 @@ router.get('/:id', asyncHandler(async (req, res) => {
     return res.json(image);
 }));
 
+router.put('/:id', imageValidations.validateCreate, asyncHandler(async (req, res) => {
+    const id = req.body.id;
+    delete req.body.id;
+    await Image.update(req.body, {
+        where: { id },
+        returning: true,
+        plain: true
+    });
+    const image = await Image.findbyPk(id);
+    return res.json(image);
+}));
+
+router.delete('/:id', asyncHandler(async (req, res) => {
+    const image = await Image.findbyPk(req.params.id);
+    if (!image) throw new Error("Can't find image.");
+
+    await Image.destroy({ where: { id: image.id } });
+    return res.json({ id: image.id });
+}));
+
 router.post('/new', requireAuth, asyncHandler(async (req, res) => {
     const image = await db.Image.create(req.body);
     res.json(image);
 }));
+
 
 module.exports = router;
