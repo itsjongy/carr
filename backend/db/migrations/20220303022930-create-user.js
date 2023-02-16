c8402c018a15db0d1f438aaa970a7f050a0acc3d
Jason Li <jasonjli218@gmail.com>
2023-02-15 22:48:46 -0800

    deploying to render

--- backend/db/migrations/20220303022930-create-user.js
+++ backend/db/migrations/20220303022930-create-user.js
@@ -1,4 +1,11 @@
 'use strict';
+
+let options = {};
+options.tableName = 'Users';
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.createTable('Users', {
@@ -32,9 +39,9 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue: Sequelize.fn('now')
       }
-    });
+    }, options);
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Users');
+    return queryInterface.dropTable('Users', options);
   }
 };
